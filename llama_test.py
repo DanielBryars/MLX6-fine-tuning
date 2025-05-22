eb49b94325dd484a757d54a03637054053cbc4b4
Daniel Bryars <bryars@hotmail.com>
2025-05-22 13:54:01 +0100

    WUP

--- llama_test.py
+++ llama_test.py
@@ -1,6 +1,8 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 model_id = "meta-llama/Meta-Llama-3-8B"
 
 tokenizer = AutoTokenizer.from_pretrained(model_id)
@@ -9,3 +11,16 @@ model = AutoModelForCausalLM.from_pretrained(
     torch_dtype=torch.float16,
     device_map="auto"
 )
+
+if (__name__ == "__main__"):
+    prompt = "Hello, I love you won't you tell me your name?"
+
+    inputs = tokenizer(prompt, return_tensors="pt").to(device)
+
+    outputs = model.generate(
+        **inputs,
+        max_new_tokens=50,
+        pad_token_id=tokenizer.eos_token_id
+    )
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    print(f"{model_id}:{response}")
